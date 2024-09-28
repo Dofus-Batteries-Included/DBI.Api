9d467d7eb961121565545514c79b63dd1647c9e3
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-09-28 12:06:26 +0200

    add more fields to FindPathResponse

--- Server/Features/PathFinder/Controllers/Responses/FindPathResponse.cs
+++ Server/Features/PathFinder/Controllers/Responses/FindPathResponse.cs
@@ -1,3 +1,4 @@
+using Server.Common.Models;
 using Server.Features.PathFinder.Models;
 
 namespace Server.Features.PathFinder.Controllers.Responses;
@@ -5,5 +6,9 @@ namespace Server.Features.PathFinder.Controllers.Responses;
 public class FindPathResponse
 {
     public bool FoundPath { get; set; }
+    public required long FromMapId { get; init; }
+    public required Position? FromMapPosition { get; init; }
+    public required long ToMapId { get; init; }
+    public required Position? ToMapPosition { get; init; }
     public IReadOnlyCollection<PathStep>? Steps { get; set; }
 }
