430c16538d43879012ee8bca89231f46df8769d4
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-10-06 17:36:48 +0200

    trigger dpln refresh on latest version changed

---
+++ Server/Features/TreasureSolver/TriggerDplnDataSourceRefreshOnLatestVersionChanged.cs
@@ -0,0 +1,16 @@
+﻿using DBI.Server.Common.Notifications;
+using DBI.Server.Features.TreasureSolver.Workers;
+using MediatR;
+
+namespace DBI.Server.Features.TreasureSolver;
+
+class TriggerDplnDataSourceRefreshOnLatestVersionChanged(RefreshDplnDataSource dataSource, ILogger<TriggerDplnDataSourceRefreshOnLatestVersionChanged> logger)
+    : INotificationHandler<LatestVersionChangedNotification>
+{
+    public Task Handle(LatestVersionChangedNotification notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Latest version has changed, DPLN data source will be refreshed ASAP.");
+        dataSource.Refresh();
+        return Task.CompletedTask;
+    }
+}
