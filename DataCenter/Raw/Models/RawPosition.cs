9dd07c0688bb292e91fff21a3ac113b3e9dc2a45
Ismail Bennani <ismail.lahkim.bennani@gmail.com>
2024-10-14 21:44:41 +0200

    target net standard 2.1 instead of net8.0 in library projects (#48)

--- DataCenter/Raw/Models/RawPosition.cs
+++ DataCenter/Raw/Models/RawPosition.cs
@@ -5,7 +5,27 @@
 /// </summary>
 /// <param name="X">The horizontal coordinate.</param>
 /// <param name="Y">The vertical coordinate.</param>
-public record struct RawPosition(int X, int Y);
+public readonly struct RawPosition : IEquatable<RawPosition>
+{
+    public RawPosition(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public int X { get; }
+    public int Y { get; }
+
+    public bool Equals(RawPosition other) => X == other.X && Y == other.Y;
+
+    public override bool Equals(object? obj) => obj is RawPosition other && Equals(other);
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+
+    public static bool operator ==(RawPosition left, RawPosition right) => left.Equals(right);
+
+    public static bool operator !=(RawPosition left, RawPosition right) => !left.Equals(right);
+}
 
 public static class RawPositionExtensions
 {
---
+++ PathFinder/Required.cs
@@ -0,0 +1,16 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace System.Runtime.CompilerServices;
+
+class IsExternalInit
+{
+}
+
+class RequiredMemberAttribute : Attribute
+{
+}
+
+class CompilerFeatureRequiredAttribute : Attribute
+{
+    public CompilerFeatureRequiredAttribute(string name) { }
+}
